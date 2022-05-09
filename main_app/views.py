f8da27908a4cf11c0d20617240dacb8b287c147a
flyingisfreedom5 <berkay_tor@hotmail.com>
2022-05-10 01:12:13 +0300

    Second lab commit

--- main_app/views.py
+++ main_app/views.py
@@ -1,28 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Finch
 # Create your views here.
 
-class Finch:
-    def __init__(self, name, breed, description, age):
-      self.name = name
-      self.breed = breed
-      self.description = description
-      self.age = age
-
-finches = [
-    Finch('Charlie', 'Zebra', 'Grey Shape', 0),
-    Finch('Mike', 'Gouldian', 'Blue and Yellow Shape', 3),
-    Finch('Peter', 'European', 'Black and Yellow Shape', 5),
-    Finch('Sophia', 'Java Sparrow', 'Blue Shape', 2)
-]
 
 def home(request):
-    return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>' )
+    return render(request, 'home.html' )
 
 
 def about(request):
     return render(request, 'about.html' )
 
 def finches_index(request):
+    finches = Finch.objects.all()
     return render(request, 'finches/index.html', { 'finches': finches })
+
+def finches_detail(request, finch_id):
+    finch = Finch.objects.get(id=finch_id)
+    return render(request, 'finches/detail.html', { 'finch': finch })
     
